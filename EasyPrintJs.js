1ad32cbb75927afb00ac100702863ef1f910ea49
adilrasheedkhan <adilrasheedkhan4@gmail.com>
2018-01-30 16:32:48 +0500

    Add files via upload

---
+++ EasyPrintJs.js
@@ -0,0 +1,66 @@
+(function()
+{
+	L.Control.EasyPrint = L.Control.extend(
+	{
+		options: 
+		{
+		title: 'Print Map',
+		position: 'topright',
+        //icon: (function(){ return document.createElement('span'); }),
+		id:             null,       // an id to tag the Bar with
+		leafletClasses: true        // use leaflet classes?
+		},
+        
+        
+      
+         
+		onAdd: function () 
+		{
+		var container = L.DomUtil.create('div', 'leaflet-control-easyPrint leaflet-bar leaflet-control');
+
+		this.link = L.DomUtil.create('a', 'leaflet-control-easyPrint-button leaflet-bar-part', container);
+		this.link.id = "leafletEasyPrint";
+		this.link.title = this.options.title;
+
+		L.DomEvent.addListener(this.link, 'click', printPage, this.options);
+
+		return container;
+		}
+
+	});
+
+		L.easyPrint = function(options)
+		{
+			return new L.Control.EasyPrint(options);
+		};
+
+		function printPage()
+		{
+
+			if (this.elementsToHide)
+			{
+				var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);  
+				for (var i = 0; i < htmlElementsToHide.length; i++) {
+					htmlElementsToHide[i].className = htmlElementsToHide[i].className + ' _epHidden';
+				}
+			}
+			window.print();
+
+			if (this.elementsToHide){
+				var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);  
+
+				for (var i = 0; i < htmlElementsToHide.length; i++) {
+				htmlElementsToHide[i].className = htmlElementsToHide[i].className.replace(' _epHidden','');
+				}
+			}
+
+
+		}
+})();
+    
+
+
+	
+	
+	
+	
